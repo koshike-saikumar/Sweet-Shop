9482edd98f3e9b2f7bfd779c72ac8cfd1aa02a75
koshike-saikumar <koshikesai25@gmail.com>
2025-07-04 11:36:19 +0530

    update to 19

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,7 +4,7 @@ import { BiUser } from "react-icons/bi";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 // import { useDispatch } from "react-redux";
 // import { Helmet } from "react-helmet-async";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 // import { doc, getDoc } from "firebase/firestore";
 // import { auth, db } from "../config/firebase";
@@ -17,113 +17,115 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   // const navigate = useNavigate();
-//   const dispatch = useDispatch();
+  //   const dispatch = useDispatch();
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
-//   const signinUser = async (e) => {
-//     e.preventDefault();
-//     setLoading(true);
+  const signinUser = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    toast.success("Welcome! Login Successful");
+        setLoading(false);
 
-//     try {
-//       const userCredential = await signInWithEmailAndPassword(
-//         auth,
-//         email,
-//         password
-//       );
-//       const user = userCredential.user;
+    // try {
+    //   const userCredential = await signInWithEmailAndPassword(
+    //     auth,
+    //     email,
+    //     password
+    //   );
+    //   const user = userCredential.user;
 
-//       const userRef = doc(db, "users", user.uid);
-//       const userDoc = await getDoc(userRef);
+    //   const userRef = doc(db, "users", user.uid);
+    //   const userDoc = await getDoc(userRef);
 
-//       if (userDoc.exists()) {
-//         const userData = userDoc.data();
-//         dispatch(
-//           setActiveUser({
-//             email: user.email,
-//             userId: user.uid,
-//             isSpecialMember: userData.isSpecialMember ?? false,
-//           })
-//         );
-//         dispatch(setCart(userData.cart || []));
-//       } else {
-//         dispatch(
-//           setActiveUser({
-//             email: user.email,
-//             userId: user.uid,
-//             isSpecialMember: false,
-//           })
-//         );
-//       }
-//       toast.success("Welcome! Login Successful");
-//       navigate("/");
-//     } catch (error) {
-//       console.error("Login error:", error);
-//       if (
-//         error.code === "auth/user-not-found" ||
-//         error.code === "auth/wrong-password"
-//       ) {
-//         toast.error("Invalid email or password. Please try again.");
-//       } else {
-//         toast.error("Login failed. Please try again later.");
-//       }
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+    //   if (userDoc.exists()) {
+    //     const userData = userDoc.data();
+    //     dispatch(
+    //       setActiveUser({
+    //         email: user.email,
+    //         userId: user.uid,
+    //         isSpecialMember: userData.isSpecialMember ?? false,
+    //       })
+    //     );
+    //     dispatch(setCart(userData.cart || []));
+    //   } else {
+    //     dispatch(
+    //       setActiveUser({
+    //         email: user.email,
+    //         userId: user.uid,
+    //         isSpecialMember: false,
+    //       })
+    //     );
+    //   }
+    //   toast.success("Welcome! Login Successful");
+    //   navigate("/");
+    // } catch (error) {
+    //   console.error("Login error:", error);
+    //   if (
+    //     error.code === "auth/user-not-found" ||
+    //     error.code === "auth/wrong-password"
+    //   ) {
+    //     toast.error("Invalid email or password. Please try again.");
+    //   } else {
+    //     toast.error("Login failed. Please try again later.");
+    //   }
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
 
-//   const guestLogin = async () => {
-//     setLoading(true);
+  //   const guestLogin = async () => {
+  //     setLoading(true);
 
-//     try {
-//       const guestEmail = "user@user.com";
-//       const guestPassword = "1234567890";
-//       const response = await signInWithEmailAndPassword(
-//         auth,
-//         guestEmail,
-//         guestPassword
-//       );
-//       const user = response.user;
+  //     try {
+  //       const guestEmail = "user@user.com";
+  //       const guestPassword = "1234567890";
+  //       const response = await signInWithEmailAndPassword(
+  //         auth,
+  //         guestEmail,
+  //         guestPassword
+  //       );
+  //       const user = response.user;
 
-//       if (user) {
-//         const userRef = doc(db, "users", user.uid);
-//         const userDoc = await getDoc(userRef);
+  //       if (user) {
+  //         const userRef = doc(db, "users", user.uid);
+  //         const userDoc = await getDoc(userRef);
 
-//         if (userDoc.exists()) {
-//           const userData = userDoc.data();
-//           dispatch(
-//             setActiveUser({
-//               email: user.email,
-//               userId: user.uid,
-//               isSpecialMember: userData.isSpecialMember ?? false,
-//             })
-//           );
-//           dispatch(setCart(userData.cart || []));
-//         } else {
-//           dispatch(
-//             setActiveUser({
-//               email: user.email,
-//               userId: user.uid,
-//               isSpecialMember: false,
-//             })
-//           );
-//         }
-//         toast.success("Welcome! Guest Login Successful");
-//         navigate("/");
-//       } else {
-//         console.log("User object is null");
-//       }
-//     } catch (error) {
-//       console.error("Guest login error:", error);
-//       toast.error("An error occurred during guest login.");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+  //         if (userDoc.exists()) {
+  //           const userData = userDoc.data();
+  //           dispatch(
+  //             setActiveUser({
+  //               email: user.email,
+  //               userId: user.uid,
+  //               isSpecialMember: userData.isSpecialMember ?? false,
+  //             })
+  //           );
+  //           dispatch(setCart(userData.cart || []));
+  //         } else {
+  //           dispatch(
+  //             setActiveUser({
+  //               email: user.email,
+  //               userId: user.uid,
+  //               isSpecialMember: false,
+  //             })
+  //           );
+  //         }
+  //         toast.success("Welcome! Guest Login Successful");
+  //         navigate("/");
+  //       } else {
+  //         console.log("User object is null");
+  //       }
+  //     } catch (error) {
+  //       console.error("Guest login error:", error);
+  //       toast.error("An error occurred during guest login.");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
   return (
     <>
@@ -141,9 +143,9 @@ const Login = () => {
           <div className="login-form-container">
             <div>
               <h1 className="login-title">Login</h1>
-              <form 
-            //   onSubmit={signinUser} 
-              className="login-form">
+              <form
+                onSubmit={signinUser}
+                className="login-form">
                 <div className="input-group">
                   <input
                     type="email"
@@ -192,15 +194,15 @@ const Login = () => {
                   // disabled={loading}
                   className="login-button"
                 >
-                  {/* {loading ? "Logging in..." : "Login"} */}
+                  {loading ? "Logging in..." : "Login"}
                 </button>
                 <button
                   type="button"
-                //   onClick={guestLogin}
+                  //   onClick={guestLogin}
                   // disabled={loading}
                   className="login-button guest-login-button"
                 >
-                  {/* {loading ? "Logging in..." : "Guest Login"} */}
+                  {loading ? "Logging in..." : "Guest Login"}
                 </button>
                 <div className="signup-link">
                   <span>
